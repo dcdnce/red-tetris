291dfdfc1ac0767ab9601cf97280e286faa70f69
dcdnce <pforesti@student.42nice.fr>
2025-05-14 17:56:49 +0200

    💄 Front testing 1st draw

--- client/src/components/ui/Header.jsx
+++ client/src/components/ui/Header.jsx
@@ -4,22 +4,13 @@ import styles from "../../styles/ui/Header.module.css";
 
 function Header() {
    return (
-      <header className={styles.header}>
-         <div className={styles.container}>
-            <Link to="/">
-               <div className={styles.container}>
-                  <div className={styles.logoSpan}></div>
-                  <h1 className={styles.title}>Tetris</h1>
-               </div>
-            </Link>
-            <nav className={styles.nav}>
-               <ul>
-                  <li className="ngos-global">
-                     <Link to="/about">About</Link>
-                  </li>
-               </ul>
-            </nav>
-         </div>
+      <header className={`${styles.header} card`}>
+         <Link to="/">
+            <div className={styles.container}>
+               <div className={styles.logoSpan}></div>
+               <h1 className={styles.title}>Tetris</h1>
+            </div>
+         </Link>
       </header>
    );
 }
